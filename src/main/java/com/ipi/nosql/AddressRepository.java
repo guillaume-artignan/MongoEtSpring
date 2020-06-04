c00f69e4df2174652a87892e3ae804e4f4d55581
guillaume.artignan@gmail.com <guillaume.artignan@gmail.com>
2020-06-04 15:57:13 +0200

    Ajout des villes

--- src/main/java/com/ipi/nosql/AddressRepository.java
+++ src/main/java/com/ipi/nosql/AddressRepository.java
@@ -1,9 +1,14 @@
 package com.ipi.nosql;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AddressRepository extends MongoRepository<Address,String> {
 
+    @Query("{nom_commune : ?0}")
+    public Page<Address> trouveParVille(Pageable p,String nom);
 }
--- src/main/java/com/ipi/nosql/BanController.java
+++ src/main/java/com/ipi/nosql/BanController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -15,7 +16,11 @@ public class BanController  {
     AddressRepository repo;
 
     @RequestMapping("/all")
-    public Page<Address> all(Pageable p){
+    public Page<Address> all(Pageable p,
+                             @RequestParam(value="ville",required = false) String ville){
+        if (ville==null)
         return repo.findAll(p);
+        else return repo.trouveParVille(p,ville);
+
     }
 }
